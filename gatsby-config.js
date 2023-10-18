da942a7b67b2406442cb14cd861de7f3d1cbfdcc
e-baron <raphael.baroni@vinci.be>
2023-10-18 12:20:28 +0200

    update: module8 reviewed

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,8 +46,7 @@ module.exports = {
           { name: 'Module 5 : React Context', link: '/modules/5' },
           { name: 'Module 6 : API REST avec MongoDB', link: '/modules/6' },
           { name: 'Module 7 : React router & Ant Design', link: '/modules/7' },
-          /*
-          { name: 'Module 8 : GraphQL', link: '/modules/8' },*/
+          { name: 'Module 8 : GraphQL', link: '/modules/8' },
           {
             name: 'Introduction à Git',
             link: '/git',

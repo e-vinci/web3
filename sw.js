441ee047ec3b3438963aec3d6f421aa11609fc51
e-baron <raphael.baroni@vinci.be>
2024-09-18 14:30:41 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,26 +27,23 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-892d7f2f9878f4d9bf7e.js"
+    "url": "webpack-runtime-286d367388cb908d5fc9.js"
   },
   {
-    "url": "styles.772755ae840af9c8b1d1.css"
+    "url": "styles.4dbd6c91ad07c6f438e6.css"
   },
   {
-    "url": "framework-b4ba2256580d048f134d.js"
+    "url": "framework-e2e3c63439412c480862.js"
   },
   {
-    "url": "cb1608f2-bc5585e91e7842ba944c.js"
+    "url": "cb1608f2-74d64ba04f9724a22a03.js"
   },
   {
-    "url": "app-f30ec3de75aac44f45bf.js"
+    "url": "app-a57db8a7a544b6a524ba.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7d5fce97dc346cfb68547a4a095ae708"
-  },
-  {
-    "url": "polyfill-e4c7027e83f84c3e370b.js"
+    "revision": "aeea128dba86525eac40292de6ef72c0"
   },
   {
     "url": "manifest.webmanifest",
@@ -160,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/web3/app-f30ec3de75aac44f45bf.js`))) {
+  if (!resources || !(await caches.match(`/web3/app-a57db8a7a544b6a524ba.js`))) {
     return await fetch(event.request)
   }
 
